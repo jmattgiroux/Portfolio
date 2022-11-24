ae63484af4e38263c7bf2304d7439348e6af4f64
jmattgiroux <jmattgiroux@gmail.com>
2022-11-24 02:33:50 -0600

    Made the function in main more general, so that it's reusable for different currency exchanges

--- c++/currency_converter/main.cpp
+++ c++/currency_converter/main.cpp
@@ -1,17 +1,20 @@
 // Main.cpp
 #include <iostream>
 
-float convertUSDToKRWon(float usd)
+float convertCurrency(float currency, float exchangeRate)
 {
-    return usd * 1326;
+    return currency * exchangeRate;
 }
+
+// TODO: Use Currying to take a higher order function and make it specific for an exchange rate
 
 int main()
 {
     std::cout << "Input USD Amount: ";
     float usd = 0;
+    float exchangeRate = 1329.00;
     std::cin >> usd;
-    float won = convertUSDToKRWon(usd);
+    float won = convertCurrency(usd, exchangeRate);
     std::cout << usd << " Dollars is equal to " << won << " Korean Won.\n";
     return 0;
 }
