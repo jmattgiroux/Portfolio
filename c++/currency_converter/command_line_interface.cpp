3cba42d7cee75642e6b556a951d8d5d4e5b08740
jmattgiroux <jmattgiroux@gmail.com>
2022-11-29 15:33:26 -0600

    Added CMakeLists.txt file, confirmed it could build the application.

--- c++/currency_converter/command_line_interface.cpp
+++ c++/currency_converter/command_line_interface.cpp
@@ -6,7 +6,12 @@
 
 // TODO: Divide up code into 3 libraries (core logic, CLI program, and GUI program)
 // Question: how to divide code up into libraries?
+
+// TODO: Go through CMake tutorials
 // Question: how to set up CMake for this project?
+// resource: https://cmake.org/cmake/help/latest/guide/tutorial/index.html
+// Tutorial Series: https://www.youtube.com/watch?v=nlKcXPUJGwA&list=PLalVdRk2RC6o5GHu618ARWh0VO0bFlif4
+// Another Tutorial Series: https://www.youtube.com/watch?v=_yFPO1ofyF0&list=PLK6MXr8gasrGmIiSuVQXpfFuE1uPT615s
 
 // TODO: Use OpenGL or GLFW to develop GUI version
 // Resources:
